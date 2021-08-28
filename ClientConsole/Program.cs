3f4a7d0c100b3e8231afbe8e006cb21462eb55ed
Vyacheslav <ggg9gg6@yandex.ru>
2021-08-28 23:14:31 +0300

    Console application: Creating a structure for building a user message + .gitignore

---
+++ ClientConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terminal.Gui;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using System.Text;
+using System.IO;
+using System.Net.Cache;
+using System.Timers;
+
+namespace DotChat
+{
+    [Serializable]
+    public class Message {
+        public string username = "";
+        public string text = "";
+        public DateTime timestamp;
+    }
+
+    class Program
+    {
+        
+    }
+}
