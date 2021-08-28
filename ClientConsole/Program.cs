fcf0c35b72a59805ee4ab7c8ec3c25aad9ceb604
Vyacheslav <ggg9gg6@yandex.ru>
2021-08-28 23:17:18 +0300

    Console application: Creating the top menu of the application

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -19,6 +19,38 @@ namespace DotChat
 
     class Program
     {
-        
+        private static MenuBar menu;
+        private static Window winMain;
+        private static Window winMessages;
+        private static Label labelUsername;
+        private static Label labelMessage;
+        private static TextField fieldUsername;
+        private static TextField fieldMessage;
+        private static Button btnSend;
+
+        private static List<Message> messages = new List<Message>();
+
+        static void Main(string[] args)
+        {
+            Application.Init();
+
+            //ColorScheme colorDark = new ColorScheme();
+            //colorDark.Normal = new Terminal.Gui.Attribute(Color.White, Color.DarkGray);
+
+            // Создание верхнего меню приложения
+            menu = new MenuBar(new MenuBarItem[]
+            {
+                new MenuBarItem("_App", new MenuItem[]
+                {
+                    new MenuItem("_Quit", "Close the app", Application.RequestStop),
+                }),
+            })
+            {
+                X = 0, Y = 0,
+                Width = Dim.Fill(),
+                Height = 1,
+            };
+            Application.Top.Add(menu);
+        }
     }
 }
