b8469e741703fd80db6f28c7e393c9ef84e4b849
Vyacheslav <ggg9gg6@yandex.ru>
2021-08-28 22:59:26 +0300

    Implementation of the main user dialog box

--- ClientGUI/MainForm.Designer.cs
+++ ClientGUI/MainForm.Designer.cs
@@ -1,1 +1,168 @@
-﻿
+﻿namespace DotChatWF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listMessages = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fieldUsername = new System.Windows.Forms.TextBox();
+            this.fieldMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.updateLoop = new System.Windows.Forms.Timer(this.components);
+            this.btnAuth = new System.Windows.Forms.Button();
+            this.btnReg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listMessages
+            // 
+            this.listMessages.FormattingEnabled = true;
+            this.listMessages.ItemHeight = 16;
+            this.listMessages.Location = new System.Drawing.Point(12, 12);
+            this.listMessages.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listMessages.Name = "listMessages";
+            this.listMessages.Size = new System.Drawing.Size(776, 340);
+            this.listMessages.TabIndex = 0;
+            this.listMessages.SelectedIndexChanged += new System.EventHandler(this.listMessages_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 422);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Message";
+            // 
+            // fieldUsername
+            // 
+            this.fieldUsername.AutoCompleteCustomSource.AddRange(new string[] {"You are not logged in"});
+            this.fieldUsername.Location = new System.Drawing.Point(108, 374);
+            this.fieldUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.fieldUsername.Name = "fieldUsername";
+            this.fieldUsername.ReadOnly = true;
+            this.fieldUsername.Size = new System.Drawing.Size(355, 22);
+            this.fieldUsername.TabIndex = 3;
+            this.fieldUsername.Text = "You are not logged in";
+            this.fieldUsername.TextChanged += new System.EventHandler(this.fieldUsername_TextChanged);
+            // 
+            // fieldMessage
+            // 
+            this.fieldMessage.Location = new System.Drawing.Point(108, 409);
+            this.fieldMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.fieldMessage.Multiline = true;
+            this.fieldMessage.Name = "fieldMessage";
+            this.fieldMessage.Size = new System.Drawing.Size(520, 41);
+            this.fieldMessage.TabIndex = 4;
+            this.fieldMessage.TextChanged += new System.EventHandler(this.fieldMessage_TextChanged);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(644, 374);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(144, 78);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // updateLoop
+            // 
+            this.updateLoop.Interval = 1000;
+            this.updateLoop.Tick += new System.EventHandler(this.updateLoop_Tick);
+            // 
+            // btnAuth
+            // 
+            this.btnAuth.Location = new System.Drawing.Point(234, 460);
+            this.btnAuth.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAuth.Name = "btnAuth";
+            this.btnAuth.Size = new System.Drawing.Size(141, 28);
+            this.btnAuth.TabIndex = 6;
+            this.btnAuth.Text = "Authentification";
+            this.btnAuth.UseVisualStyleBackColor = true;
+            this.btnAuth.Click += new System.EventHandler(this.btnAuth_Click);
+            // 
+            // btnReg
+            // 
+            this.btnReg.Location = new System.Drawing.Point(428, 460);
+            this.btnReg.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(141, 28);
+            this.btnReg.TabIndex = 7;
+            this.btnReg.Text = "Registration";
+            this.btnReg.UseVisualStyleBackColor = true;
+            this.btnReg.Click += new System.EventHandler(this.btnReg_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 497);
+            this.Controls.Add(this.btnReg);
+            this.Controls.Add(this.btnAuth);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.fieldMessage);
+            this.Controls.Add(this.fieldUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listMessages);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.VisibleChanged += new System.EventHandler(this.MainForm_VisibleChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listMessages;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fieldUsername;
+        private System.Windows.Forms.TextBox fieldMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Timer updateLoop;
+    private System.Windows.Forms.Button btnAuth;
+    private System.Windows.Forms.Button btnReg;
+  }
+}
